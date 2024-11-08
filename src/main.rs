acb0bc743963ccd576e478e3b75fb0d52eaff203
Anton Nomed <anton@MacBook-Air-6.local>
2024-11-08 18:27:06 +0100

    Version corrigée

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,27 @@
+// main.rs
 #![no_std]
 #![no_main]
 
-use core::ptr;
-use avr_device::entry;
-use panic_halt as _;
+mod delay;
+mod gpio;
 
-const DDRB: *mut u8 = 0x24 as *mut u8;
-const PORTB: *mut u8 = 0x25 as *mut u8;
-const PINB5: u8 = 1 << 5;
+use avr_device::entry;
+use gpio::gpio::{configure_pin, write_pin, Mode};
+use delay::delay::delay_ms;
 
 #[entry]
 fn main() -> ! {
-    setup();
+    configure_pin(5, Mode::Output);
 
     loop {
-        led_on();
+        write_pin(5, true);
         delay_ms(1000);
-        led_off();
+        write_pin(5, false);
         delay_ms(1000);
     }
 }
 
-fn setup() {
-    unsafe {
-        let current_ddrb = ptr::read_volatile(DDRB);
-        ptr::write_volatile(DDRB, current_ddrb | PINB5);
-    }
-}
 
-fn led_on() {
-    unsafe {
-        let current_portb = ptr::read_volatile(PORTB);
-        ptr::write_volatile(PORTB, current_portb | PINB5);
-    }
-}
 
-fn led_off() {
-    unsafe {
-        let current_portb = ptr::read_volatile(PORTB);
-        ptr::write_volatile(PORTB, current_portb & !PINB5);
-    }
-}
 
-fn delay_ms(ms: u16) {
-    for _ in 0..ms {
-        for _ in 0..16_000 {
-            unsafe { ptr::read_volatile(0x00 as *const u8); }
-        }
-    }
-}
+
